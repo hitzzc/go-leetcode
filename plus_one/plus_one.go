8fc01977bf914f31e91554540f7bcd8e7f7c5cb9
zczou <hitzzc@126.com>
2016-09-25 14:33:59 +0800

    plus_one

---
+++ plus_one/plus_one.go
@@ -0,0 +1,18 @@
+package plus_one
+
+func plusOne(digits []int) []int {
+	carry := 1
+	for i := len(digits); i >= 0; i++ {
+		sum := carry + digits[i]
+		if sum < 10 {
+			digits[i] = sum
+			return digits
+		}
+		digits[i] = sum % 10
+		carry = 1
+	}
+	if carry == 1 {
+		return append([]int{carry}, digits...)
+	}
+	return digits
+}

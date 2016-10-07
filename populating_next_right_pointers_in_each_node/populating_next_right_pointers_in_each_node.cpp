17b597b736c9b0a8703c9f81bba3e55210321fa5
zczou <hitzzc@126.com>
2016-10-07 22:22:36 +0800

    populating_next_right_pointers_in_each_node/

---
+++ populating_next_right_pointers_in_each_node/populating_next_right_pointers_in_each_node.cpp
@@ -0,0 +1,24 @@
+struct TreeLinkNode {
+ 	int val;
+ 	TreeLinkNode *left, *right, *next;
+ 	TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ };
+ 
+void connect(TreeLinkNode *root){
+	if (!root){
+		return;
+	}
+	TreeLinkNode* currentNode;
+	while (root->left != NULL){
+		currentNode = root;
+		while (currentNode != NULL){
+			currentNode->left->next = currentNode->right;
+			if (currentNode->next != NULL){
+				currentNode->right->next = currentNode->next->left;
+			}
+			currentNode = currentNode->next;
+		}
+		root = root->left;
+	}
+	return;
+}

75159d2f16cf18fda6602ea7112f6d98f31d7ed3
zczou <hitzzc@126.com>
2016-10-11 12:24:49 +0800

    single_number_II

--- single_number_II/single_number_II.go
+++ single_number_II/single_number_II.go
@@ -3,11 +3,9 @@ package single_number_II
 func singleNumber(nums []int) int {
 	var one, two, three int
 	for i := range nums {
-		two |= one & nums[i]
-		one ^= nums[i]
-		three = one & two
-		one &= ^three
-		two &= ^three
+		three = two & nums[i]
+		two = ^three & (two | one&nums[i])
+		one = ^three & (one ^ nums[i])
 	}
 	return one
 }

cd90839bd0dbc782ac8034b397b47dac9defc5f5
zczou <hitzzc@126.com>
2016-10-19 14:19:59 +0800

    search_a_2D_matrix_II

---
+++ search_a_2D_matrix_II/search_a_2D_matrix_II.go
@@ -0,0 +1,20 @@
+package search_a_2D_matrix_II
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	row, col := 0, len(matrix[0])-1
+	for row <= len(matrix)-1 && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		}
+		for row <= len(matrix)-1 && matrix[row][col] < target {
+			row++
+		}
+		for col >= 0 && matrix[row][col] > target {
+			col--
+		}
+	}
+	return false
+}

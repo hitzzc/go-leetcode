8547e55f43fbdc77fb327ebac51ad612f99bee13
Sandy <openset.wang@gmail.com>
2018-12-10 16:48:38 +0800

    Update remove_element.go

--- remove_element/remove_element.go
+++ remove_element/remove_element.go
@@ -1,13 +1,13 @@
 package remove_element
 
 func removeElement(nums []int, val int) int {
-	start := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == val {
-			continue
-		}
-		nums[start] = nums[i]
-		start++
-	}
-	return start
+    l := 0
+    for _, v := range nums {
+        if v != val {
+            nums[l] = v
+            l++
+        }
+    }
+
+    return l
 }

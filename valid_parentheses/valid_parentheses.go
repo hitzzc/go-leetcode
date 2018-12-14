230388b0835578bd555c0f9e9d6c0532e60347d0
Sandy <openset.wang@gmail.com>
2018-12-14 11:38:36 +0800

    Update valid_parentheses.go

--- valid_parentheses/valid_parentheses.go
+++ valid_parentheses/valid_parentheses.go
@@ -1,49 +1,26 @@
 package valid_parentheses
 
 func isValid(s string) bool {
-	if len(s)%2 != 0 {
-		return false
-	}
-	left := map[byte]struct{}{
-		'(': struct{}{},
-		'[': struct{}{},
-		'{': struct{}{},
-	}
-	right := map[byte]struct{}{
-		')': struct{}{},
-		']': struct{}{},
-		'}': struct{}{},
-	}
-	bytes := []byte(s)
-	stack := []byte{}
-	for i := range bytes {
-		if _, ok := left[bytes[i]]; ok {
-			stack = append(stack, bytes[i])
-		} else if _, ok = right[bytes[i]]; ok {
-			if len(stack) == 0 {
-				return false
-			}
-			switch stack[len(stack)-1] {
-			case '(':
-				if bytes[i] != ')' {
-					return false
-				}
-			case '[':
-				if bytes[i] != ']' {
-					return false
-				}
-			case '{':
-				if bytes[i] != '}' {
-					return false
-				}
-			}
-			stack = stack[:len(stack)-1]
-		} else {
-			return false
-		}
-	}
-	if len(stack) == 0 {
-		return true
-	}
-	return false
+    m := map[rune]rune{
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+    stack := make([]rune, len(s))
+    top := 0
+    for _, c := range s {
+        switch c {
+        case '(', '[', '{':
+            stack[top] = m[c]
+            top++
+        case ')', ']', '}':
+            if top > 0 && stack[top-1] == c {
+                top--
+            } else {
+                return false
+            }
+        }
+    }
+
+    return top == 0
 }
